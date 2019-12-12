aa45e973f5f11361dade44fbdb9ddaa645375ec5
Semyon Komyagin <komyagin.s@gmail.com>
2019-12-12 14:03:41 +0300

    Initial commit

---
+++ src/main/java/io/github/komyagin/model/Notice.java
@@ -0,0 +1,4 @@
+package io.github.komyagin.model;
+
+public class Notice {
+}
---
+++ src/main/resources/sql/ddl.sql
@@ -0,0 +1,24 @@
+create schema if not exists lab;
+
+create table if not exists lab.person
+(
+    id         serial       not null primary key,
+    first_name VARCHAR(100) not null,
+    last_name  VARCHAR(100) not null,
+    email      VARCHAR(100) not null unique,
+    category   text
+);
+
+create table if not exists lab.notice
+(
+    id           serial        not null primary key,
+    person_id    int           not null,
+    header       VARCHAR(100)  not null,
+    body         VARCHAR(1000) not null,
+    tel_number   VARCHAR(100)  not null,
+    created_date timestamp     not null,
+    updated_date timestamp     not null,
+    category     text,
+    foreign key (person_id) references lab.person (id)
+);
+
