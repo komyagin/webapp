9a7c1f5379db43fa1104fad8db2b529731c76715
Semyon Komyagin <komyagin.s@gmail.com>
2020-01-03 21:18:35 +0300

    Added RESTful controllers tests

---
+++ src/test/java/io/github/komyagin/controller/NoticeControllerTest.java
@@ -0,0 +1,26 @@
+package io.github.komyagin.controller;
+
+import org.junit.jupiter.api.Test;
+
+class NoticeControllerTest {
+
+    @Test
+    void getNotices() {
+    }
+
+    @Test
+    void getNotice() {
+    }
+
+    @Test
+    void addNotice() {
+    }
+
+    @Test
+    void updateNotice() {
+    }
+
+    @Test
+    void removeNotice() {
+    }
+}
---
+++ src/test/java/io/github/komyagin/controller/PersonControllerTest.java
@@ -0,0 +1,30 @@
+package io.github.komyagin.controller;
+
+import org.junit.jupiter.api.Test;
+
+class PersonControllerTest {
+
+    @Test
+    void getPerson() {
+    }
+
+    @Test
+    void testGetPerson() {
+    }
+
+    @Test
+    void getPersonsNotices() {
+    }
+
+    @Test
+    void addPerson() {
+    }
+
+    @Test
+    void updatePerson() {
+    }
+
+    @Test
+    void deletePerson() {
+    }
+}
